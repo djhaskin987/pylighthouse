4275024a78448629992f9a07170021433d6cc585
Daniel Jay Haskin <djhaskin987@gmail.com>
2018-12-27 12:38:37 -0700

    Add copyright notices

--- pylighthouse/pylighthouse.py
+++ pylighthouse/pylighthouse.py
@@ -1,10 +1,21 @@
 # -*- coding: utf-8 -*-
+# Copyright 2018 the pylighthouse authors, see the AUTHORS.rst file.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
 
 """Main module."""
 
 from sortedcontainers import SortedDict
-import math
-
 
 class LighthouseException(Exception):
     pass
@@ -139,7 +150,7 @@ class Node(object):
         self.assigned_workloads = dict()
 
     def add_ward(self, ward):
-        self.resources[ward] = -math.inf
+        self.resources[ward] = -float("inf")
 
 
 class Distributor(object):
